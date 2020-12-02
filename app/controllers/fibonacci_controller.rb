02eba54028c810a65a92d30dc0c3e2be5cb1ffc1
Viktor Emilov <kiwispooon@gmail.com>
2020-12-02 12:21:21 +0000

    starting point

--- app/controllers/fibonacci_controller.rb
+++ app/controllers/fibonacci_controller.rb
@@ -1,4 +1,6 @@
+
 class FibonacciController < ApplicationController
+    
 
     post '/fibonacci/inspect' do
        @fibonacci = Fibonacci.create(sequence: params[:sequence])
@@ -7,7 +9,17 @@ class FibonacciController < ApplicationController
 
     get '/fibonacci/:id' do 
         @fibonacci = Fibonacci.find(params[:id])
+
+        def fib(places)
+            a = 0
+            b = 1    
+            while b < places do
+                puts b         
+                a,b = b,a+b
+            end   
+        end
+
         erb :fibonacci
     end
-
+    
 end
